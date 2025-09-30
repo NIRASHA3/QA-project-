bddc3e8470a48a178fd580681d850a1d4c168af9
NIRASHA3 <nirashadeshani3@gmail.com>
2025-09-30 17:39:37 +0530

    TDD/BDD testing workflow

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,6 +9,7 @@ const login = async (req, res) => {
     if (!req.body.password || req.body.password.length < 8) {
       return res.status(400).json({ message: "Password must be at least 8 characters long" });
     }*/
+   
 
     // Find user with matching userId, password, and verified status
     const user = await UserModel.findOne({
